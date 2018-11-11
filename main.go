addcdd09b6124d9108439a8e1b00600bea5dd263
Brian Dhang <dangbh1002@gmail.com>
2018-11-11 22:41:11 +0700

    Added https

--- main.go
+++ main.go
@@ -2,13 +2,16 @@ package main
 
 import (
 	"github.com/labstack/echo"
+	"golang.org/x/crypto/acme/autocert"
 )
 
 func main() {
 	e := echo.New()
+
+	e.AutoTLSManager.Cache = autocert.DirCache("/var/www/.cache")
 	e.Static("/static", "./product/static")
 	e.GET("*", func(c echo.Context) error {
 		return c.File("./product/index.html")
 	})
-	e.Start(":80")
+	e.StartAutoTLS(":443")
 }
