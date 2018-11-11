e43c7dda58e8694ca5df2d53118d22e5ad77e722
Brian Dhang <dangbh1002@gmail.com>
2018-11-12 00:20:12 +0700

    Revert to http

--- main.go
+++ main.go
@@ -2,16 +2,22 @@ package main
 
 import (
 	"github.com/labstack/echo"
-	"golang.org/x/crypto/acme/autocert"
+	// "golang.org/x/crypto/acme/autocert"
 )
 
 func main() {
 	e := echo.New()
 
-	e.AutoTLSManager.Cache = autocert.DirCache("/var/www/.cache")
+	// e.AutoTLSManager.Cache = autocert.DirCache("/var/www/.cache")
+	// e.Static("/static", "./product/static")
+	// e.GET("*", func(c echo.Context) error {
+	// 	return c.File("./product/index.html")
+	// })
+	// e.StartAutoTLS(":443")
+
 	e.Static("/static", "./product/static")
 	e.GET("*", func(c echo.Context) error {
 		return c.File("./product/index.html")
 	})
-	e.StartAutoTLS(":443")
+	e.Start(":80")
 }
